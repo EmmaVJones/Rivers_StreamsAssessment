213584b2a6d6a3f07b9548b8830394198191d2f2
EmmaVJones <emma.v.jones@icloud.com>
2018-12-14 08:36:47 -0500

    starting on app v1

---
+++ R&S_app_v1/server.R
@@ -0,0 +1,69 @@
+# Run in R 3.5.1
+source('global.R')
+
+fakeConventionals <- read.csv('exampleData/fakeConventionals.csv') %>%
+  dplyr::select(FDT_STA_ID, STA_DESC, Basin, Huc6_Huc_8_Name,
+          Huc6_Name, Huc6_Vahu5, Huc6_Vahu6) %>%
+  distinct(FDT_STA_ID, .keep_all = T)
+
+x <- data.frame(x = c('StationID','Station Description',
+                      'Level 1 Code', 'Level 3 Code', 'SPG Code', 
+                      'ID305B_1', 'ID305B_3',
+                      'ID305B_3','Station Type 1', 
+                      'Station Type 2', 'Station Type 3', 'WQS Section',
+                      'WQS Class', 'Special Standards',
+                      'WQS Description'),
+                y = c('XXXXXX', 'Route XXX bridge','STREAM','AWTSHD', 'AW', 
+                      'VAW-J01R_JMS01A00','VAW-J01R_JMS01A01',
+                      'VAW-J01R_JMS01A02','AW',  'B',  'PA', '2','IV','PWS',
+                      'James River from blah to blah'))
+names(x) <- c(' ',"  ")
+
+shinyServer(function(input, output, session) {
+  
+  # display the loading feature until data loads into app
+  load_data()
+  
+  ## Assessment Unit Tab
+  output$VAmap <- renderLeaflet({
+    
+    leaflet() %>% setView(-79.2,37.7,zoom=7)%>%
+      addProviderTiles(providers$OpenStreetMap,group='Open Street Map')%>%
+      addProviderTiles(providers$Esri.WorldImagery,group='Esri World Imagery')%>%
+      addProviderTiles(providers$Stamen.TerrainBackground,group='Stamen Terrain Background')%>%
+      addLayersControl(baseGroups=c('Open Street Map','Esri World Imagery','Stamen Terrain Background'),
+                       #overlayGroups=c('Lake Monitoring Stations','All Lake Monitoring Stations'),
+                       options=layersControlOptions(collapsed=T),
+                       position='topleft')%>%
+      #addHomeButton(extent(lakeStations_shp),"All Lake Monitoring Stations")%>%
+      mapview::addMouseCoordinates(style='basic')
+  })
+  
+  
+  output$allStationsInVAHUC6 <- DT::renderDataTable({
+    DT::datatable(fakeConventionals, escape=F, rownames = F,
+                  options=list(dom='Bt',scrollX = TRUE,pageLength = nrow(x), scrollY = "200px"))})
+  
+  ## Individual Station Review Tab
+  
+  output$stationInfo <- DT::renderDataTable({
+    
+    DT::datatable(x, escape=F, rownames = F,
+                  options=list(dom='Bt',scrollX = TRUE,pageLength = nrow(x), scrollY = "200px"))
+  })
+  
+  
+  output$stationMap <- renderLeaflet({
+    
+    leaflet() %>% setView(-79.2,37.7,zoom=12)%>%
+      addProviderTiles(providers$OpenStreetMap,group='Open Street Map')%>%
+      addProviderTiles(providers$Esri.WorldImagery,group='Esri World Imagery')%>%
+      addProviderTiles(providers$Stamen.TerrainBackground,group='Stamen Terrain Background')%>%
+      addLayersControl(baseGroups=c('Open Street Map','Esri World Imagery','Stamen Terrain Background'),
+                       #overlayGroups=c('Lake Monitoring Stations','All Lake Monitoring Stations'),
+                       options=layersControlOptions(collapsed=T),
+                       position='topleft')%>%
+      #addHomeButton(extent(lakeStations_shp),"All Lake Monitoring Stations")%>%
+      mapview::addMouseCoordinates(style='basic')
+  })
+})
